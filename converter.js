06d4b3526adb8ebb47c6de6c09c5c98ff52ae1dd
grace <kellygrs@gmail.com>
2025-06-10 21:57:19 +0200

    variables

--- converter.js
+++ converter.js
@@ -1,11 +1,12 @@
 
 let converter = document.querySelector("#convert");
 
-let resultado = document.querySelector("#result");
+
 
 converter.addEventListener("click", function () {
 let n = document.querySelector("#number").value;
 let unit = document.querySelector("#units").value;
+let resultado = document.querySelector("#result");
     if (unit == "Km") {
        let result = (n/1.6093445);
         resultado.innerHTML = result + " Millas";
