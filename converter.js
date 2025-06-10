e1e8acdc315691f48112c22e7603d8056b1cf46c
grace <kellygrs@gmail.com>
2025-06-10 21:56:17 +0200

    correcciones

--- converter.js
+++ converter.js
@@ -7,13 +7,13 @@ converter.addEventListener("click", function () {
 let n = document.querySelector("#number").value;
 let unit = document.querySelector("#units").value;
     if (unit == "Km") {
-       let result = (n*1.6093445);
+       let result = (n/1.6093445);
         resultado.innerHTML = result + " Millas";
       
 
     }
     else {
-       let result = (n/1.6093445);
+       let result = (n*1.6093445);
         resultado.innerHTML = result + " Kilómetros";
      
     }
